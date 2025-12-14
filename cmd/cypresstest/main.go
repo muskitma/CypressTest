096546c3ad341b8de1d4aecf9429acd70a810634
Muski <143282825+muskitma@users.noreply.github.com>
2025-12-14 23:02:49 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/cypresstest/main.go
@@ -0,0 +1,23 @@
+// cmd/cypresstest/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "cypresstest/internal/cypresstest"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := cypresstest.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
